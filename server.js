4aa7958df3d573747913e7af6ea52d2757b79dd0
Bill Gates <bg@test.com>
2019-02-01 11:40:30 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.port || 3000;
 
 let app = express();
 
@@ -25,11 +25,11 @@ app.use((req, res, next) =>{
 
 });
 
-app.use((req, res, next)=>{
-    res.render('maintenance.hbs');
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs');
 
 
-});
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -72,6 +72,6 @@ app.get('/bad', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port ${port}');
 });
